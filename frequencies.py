accacd1f67d17193d36cf38b3b9c04bdacd906cd
Splagon <misha.higham@hotmail.co.uk>
2022-09-30 17:53:15 +0100

    Finish work

--- frequencies.py
+++ frequencies.py
@@ -11,14 +11,3 @@ def frequencies(items):
             frequencies[element] = 1;
         print(frequencies)
     return frequencies
-
-
-input = ['0', 4,4,'4','d','d','e',0,'a','d','4']
-output = frequencies(input)
-assert output['4'] == 4
-assert output['d'] == 3
-assert output['e'] == 1
-assert output['a'] == 1
-assert output['0'] == 2
-assert 4 not in output.keys()
-assert 0 not in output.keys()
